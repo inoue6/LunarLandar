0b71dbaea73135e52652efc5171199461be4e6c1
井上裕太 <inoue@alfasystem.co.jp>
2015-05-25 13:34:03 +0900

    拡大バグ取り

--- LunarLandar/LunarLandar/Assets/Source/Moon.cs
+++ LunarLandar/LunarLandar/Assets/Source/Moon.cs
@@ -3,10 +3,11 @@ using System.Collections;
 
 public class Moon : MonoBehaviour {
 	public CameraControl m_camera;
+	int m_collisionCount;
 
 	// Use this for initialization
 	void Start () {
-	
+		m_collisionCount = 0;
 	}
 	
 	// Update is called once per frame
@@ -20,13 +21,15 @@ public class Moon : MonoBehaviour {
 		if (collider.gameObject.tag == "MainCamera") {
 			m_camera.ZoomInCamera ();
 		}
+		m_collisionCount++;
 	}
 
 	// コライダー離れた
 	void OnTriggerExit2D (Collider2D collider) {
 		// カメラ縮小.
-		if (collider.gameObject.tag == "MainCamera") {
+		if (collider.gameObject.tag == "MainCamera" && m_collisionCount == 0) {
 			m_camera.ZoomOutCamera ();
 		}
+		m_collisionCount--;
 	}
 }
